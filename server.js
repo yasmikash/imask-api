3468be4b8a5749c802f097a2216e4ed5ca34f410
yasmikash <yashtricksz@gmail.com>
2022-04-28 23:44:55 +0530

    azure app service conf #2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.NODE_ENV || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/test", (req, res, next) => {
   res.json({ message: "it works" });
 });
 
-app.listen(PORT, () => console.log("app is running"));
+app.listen(PORT, () => console.log("app is running on port=" + PORT));
