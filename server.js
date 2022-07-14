c9f82a570042eb4459bb7eeb30954c62a19cf92b
yasmikash <yashtricksz@gmail.com>
2022-07-14 21:32:30 +0530

    change default port

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 8080;
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
